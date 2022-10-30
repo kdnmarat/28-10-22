9a9b0b3bbe3cdcecf29bfed3f1ace63454c8bde4
Marat Kudinov <marat.kudinov@orioninc.com>
2022-10-30 22:25:54 +0100

    Records adding is implemented

--- WindowsFormsApp2/WindowsFormsApp2/Form1.Designer.cs
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.Designer.cs
@@ -29,22 +29,26 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.tbName = new System.Windows.Forms.TextBox();
-            this.tbStreet = new System.Windows.Forms.TextBox();
-            this.tbCity = new System.Windows.Forms.TextBox();
-            this.tbState = new System.Windows.Forms.TextBox();
-            this.tbZip = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.tbSave = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.tbSave = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbZip = new System.Windows.Forms.TextBox();
+            this.tbState = new System.Windows.Forms.TextBox();
+            this.tbCity = new System.Windows.Forms.TextBox();
+            this.tbStreet = new System.Windows.Forms.TextBox();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.sbStatus = new System.Windows.Forms.StatusStrip();
+            this.slStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.slDetails = new System.Windows.Forms.ToolStripStatusLabel();
             this.groupBox1.SuspendLayout();
-            this.groupBox2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.sbStatus.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
@@ -60,10 +64,22 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Contacts:";
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(7, 20);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(662, 461);
+            this.dataGridView1.TabIndex = 0;
+            // 
             // groupBox2
             // 
             this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.sbStatus);
             this.groupBox2.Controls.Add(this.tbSave);
             this.groupBox2.Controls.Add(this.label5);
             this.groupBox2.Controls.Add(this.label4);
@@ -82,76 +98,15 @@
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Enter contact details:";
             // 
-            // tbName
+            // tbSave
             // 
-            this.tbName.Location = new System.Drawing.Point(95, 29);
-            this.tbName.Name = "tbName";
-            this.tbName.Size = new System.Drawing.Size(178, 20);
-            this.tbName.TabIndex = 0;
-            // 
-            // tbStreet
-            // 
-            this.tbStreet.Location = new System.Drawing.Point(95, 65);
-            this.tbStreet.Name = "tbStreet";
-            this.tbStreet.Size = new System.Drawing.Size(178, 20);
-            this.tbStreet.TabIndex = 1;
-            // 
-            // tbCity
-            // 
-            this.tbCity.Location = new System.Drawing.Point(95, 101);
-            this.tbCity.Name = "tbCity";
-            this.tbCity.Size = new System.Drawing.Size(178, 20);
-            this.tbCity.TabIndex = 2;
-            // 
-            // tbState
-            // 
-            this.tbState.Location = new System.Drawing.Point(95, 137);
-            this.tbState.Name = "tbState";
-            this.tbState.Size = new System.Drawing.Size(178, 20);
-            this.tbState.TabIndex = 3;
-            // 
-            // tbZip
-            // 
-            this.tbZip.Location = new System.Drawing.Point(95, 173);
-            this.tbZip.Name = "tbZip";
-            this.tbZip.Size = new System.Drawing.Size(178, 20);
-            this.tbZip.TabIndex = 4;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 32);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(38, 13);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "Name:";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(6, 68);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(38, 13);
-            this.label2.TabIndex = 6;
-            this.label2.Text = "Street:";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(6, 104);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(27, 13);
-            this.label3.TabIndex = 7;
-            this.label3.Text = "City:";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(6, 140);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(35, 13);
-            this.label4.TabIndex = 8;
-            this.label4.Text = "State:";
+            this.tbSave.Location = new System.Drawing.Point(191, 210);
+            this.tbSave.Name = "tbSave";
+            this.tbSave.Size = new System.Drawing.Size(82, 23);
+            this.tbSave.TabIndex = 10;
+            this.tbSave.Text = "Save";
+            this.tbSave.UseVisualStyleBackColor = true;
+            this.tbSave.Click += new System.EventHandler(this.tbSave_Click);
             // 
             // label5
             // 
@@ -162,25 +117,97 @@
             this.label5.TabIndex = 9;
             this.label5.Text = "ZIP:";
             // 
-            // tbSave
+            // label4
             // 
-            this.tbSave.Location = new System.Drawing.Point(191, 210);
-            this.tbSave.Name = "tbSave";
-            this.tbSave.Size = new System.Drawing.Size(82, 23);
-            this.tbSave.TabIndex = 10;
-            this.tbSave.Text = "Save";
-            this.tbSave.UseVisualStyleBackColor = true;
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 140);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "State:";
             // 
-            // dataGridView1
+            // label3
             // 
-            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(7, 20);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(662, 461);
-            this.dataGridView1.TabIndex = 0;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(27, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "City:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Street:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Name:";
+            // 
+            // tbZip
+            // 
+            this.tbZip.Location = new System.Drawing.Point(95, 173);
+            this.tbZip.Name = "tbZip";
+            this.tbZip.Size = new System.Drawing.Size(178, 20);
+            this.tbZip.TabIndex = 4;
+            // 
+            // tbState
+            // 
+            this.tbState.Location = new System.Drawing.Point(95, 137);
+            this.tbState.Name = "tbState";
+            this.tbState.Size = new System.Drawing.Size(178, 20);
+            this.tbState.TabIndex = 3;
+            // 
+            // tbCity
+            // 
+            this.tbCity.Location = new System.Drawing.Point(95, 101);
+            this.tbCity.Name = "tbCity";
+            this.tbCity.Size = new System.Drawing.Size(178, 20);
+            this.tbCity.TabIndex = 2;
+            // 
+            // tbStreet
+            // 
+            this.tbStreet.Location = new System.Drawing.Point(95, 65);
+            this.tbStreet.Name = "tbStreet";
+            this.tbStreet.Size = new System.Drawing.Size(178, 20);
+            this.tbStreet.TabIndex = 1;
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(95, 29);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(178, 20);
+            this.tbName.TabIndex = 0;
+            // 
+            // sbStatus
+            // 
+            this.sbStatus.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.slStatus,
+            this.slDetails});
+            this.sbStatus.Location = new System.Drawing.Point(3, 462);
+            this.sbStatus.Name = "sbStatus";
+            this.sbStatus.Size = new System.Drawing.Size(273, 22);
+            this.sbStatus.TabIndex = 11;
+            // 
+            // slStatus
+            // 
+            this.slStatus.ForeColor = System.Drawing.Color.Red;
+            this.slStatus.Name = "slStatus";
+            this.slStatus.Size = new System.Drawing.Size(0, 17);
+            // 
+            // slDetails
+            // 
+            this.slDetails.Name = "slDetails";
+            this.slDetails.Size = new System.Drawing.Size(0, 17);
             // 
             // Form1
             // 
@@ -192,9 +219,11 @@
             this.Name = "Form1";
             this.Text = "28-10-22";
             this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.sbStatus.ResumeLayout(false);
+            this.sbStatus.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -215,6 +244,9 @@
         private System.Windows.Forms.TextBox tbCity;
         private System.Windows.Forms.TextBox tbStreet;
         private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.StatusStrip sbStatus;
+        private System.Windows.Forms.ToolStripStatusLabel slStatus;
+        private System.Windows.Forms.ToolStripStatusLabel slDetails;
     }
 }
 
--- WindowsFormsApp2/WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -17,5 +17,26 @@ namespace WindowsFormsApp2
             InitializeComponent();
             ContactDetails.AllRecords = new List<ContactDetails>();
         }
+
+        private void tbSave_Click(object sender, EventArgs e)
+        {
+            string name = tbName.Text;
+            string street = tbStreet.Text;
+            string city = tbCity.Text;
+            string state = tbState.Text;
+            string zip = tbZip.Text;
+            if (name.Length == 0 || street.Length == 0 || city.Length == 0 || state.Length == 0 || zip.Length == 0)
+            {
+                slStatus.ForeColor = Color.Red;
+                slStatus.Text = "ERROR!";
+                slDetails.Text = "Fields can't be empty";
+                return;
+            }
+            ContactDetails record = new ContactDetails(name,street,city,state,zip);
+            ContactDetails.AllRecords.Add(record);
+            slStatus.ForeColor = Color.Green;
+            slStatus.Text = "OK!";
+            slDetails.Text = "Record was stored";
+        }
     }
 }
